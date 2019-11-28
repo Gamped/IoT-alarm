3bae673d27ae3dc7b1876f96d5e42ed0428cec65
Gamped <mathiasgam@gmail.com>
2019-11-28 11:45:09 +0100

    Changed names of pins and defined all

--- Solution/IoT_Alarm.ino
+++ Solution/IoT_Alarm.ino
@@ -1,16 +1,23 @@
-#define HCSR04_PIN_TRIG 3
-#define HCSR04_PIN_ECHO 2
-#define LDR_PIN_SIG A3
-#define LEDR_PIN_VIN 5
-#define PIR_PIN_SIG 4
-#define RFID_PIN_RST 6
-#define RFID_PIN_SDA 10
+/* ======== Define pins ======== */
+#define ULTRASONIC_ECHO 2
+#define ULTRASONIC_TRIG 3
+#define PIR_SIG 4
+#define LED_RED 5
+#define RFID_RST 6
+#define RFID_SDA 10
+#define RFID_MOSI 11
+#define RFID_MISO 12
+#define LDR_SIG A3
+#define RTC_SDA A4
+#define RTC_SCL A5
 
+/* ======== Setup ======== */
 void setup() {
   Serial.begin(9600);
 
 }
 
+/* ======== Loop ======== */
 void loop() {
 
 }
