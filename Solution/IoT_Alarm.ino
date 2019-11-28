b356ada5e5a9d58a66cbfadc7080bc2b569c5254
Gamped <mathiasgam@gmail.com>
2019-11-28 11:31:48 +0100

    Added new main file

---
+++ Solution/IoT_Alarm.ino
@@ -0,0 +1,16 @@
+#define HCSR04_PIN_TRIG 3
+#define HCSR04_PIN_ECHO 2
+#define LDR_PIN_SIG A3
+#define LEDR_PIN_VIN 5
+#define PIR_PIN_SIG 4
+#define RFID_PIN_RST 6
+#define RFID_PIN_SDA 10
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+}
