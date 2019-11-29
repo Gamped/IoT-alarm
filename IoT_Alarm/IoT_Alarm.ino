32734d8adb205402bd671ebf1b223cb953415dad
Gamped <mathiasgam@gmail.com>
2019-11-29 11:54:10 +0100

    Added simple RFID implementation

--- IoT_Alarm/IoT_Alarm.ino
+++ IoT_Alarm/IoT_Alarm.ino
@@ -3,6 +3,8 @@
 #include "LDR.h"
 #include "Ultrasonic.h"
 #include "AlarmLight.h"
+#include <SPI.h>
+#include <MFRC522.h>
 
 /* ======== Define pins ======== */
 #define ULTRASONIC_ECHO 2
@@ -22,10 +24,14 @@ PIR pir(PIR_SIG);
 LDR ldr(LDR_SIG);
 Ultrasonic ultrasonic(ULTRASONIC_ECHO, ULTRASONIC_TRIG);
 AlarmLight alarmLight(LED_RED);
+MFRC522 mfrc522(RFID_SDA, RFID_RST);
+byte readCard[4];
 
 /* ======== Setup ======== */
 void setup() {
     Serial.begin(9600);
+    SPI.begin(); 
+    mfrc522.PCD_Init(); 
 }
 
 /* ======== Loop ======== */
@@ -34,8 +40,19 @@ void loop() {
 
     Serial.println("======= LDR =======");
     Serial.println(ldr.Read());
-    Serial.println("======= Ultrasonic =======");
+    Serial.println("======= Ultrasonic ======="); 
     Serial.println(ultrasonic.ReadMicroseconds());
     Serial.println(ultrasonic.ReadCM());
+    Serial.println("======= RFID =======");
+    Serial.println(ReadRFID());
     delay(500);
+}
+
+/* ======== Non-class functions ======== */
+
+// Function that reads from the RFID card reader if an RFID card is present
+// TODO: Move to own class & verification method to only allow verified cards
+bool ReadRFID(){
+    bool result = mfrc522.PICC_IsNewCardPresent();
+    return result;
 }
