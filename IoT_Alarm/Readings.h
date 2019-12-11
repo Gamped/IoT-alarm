6c218d2f342b6625b7c59a14e0b923517ead9742
Gamped <mathiasgam@gmail.com>
2019-12-11 16:48:27 +0100

    "Reset readings"

--- IoT_Alarm/Readings.h
+++ IoT_Alarm/Readings.h
@@ -9,45 +9,12 @@
 
 #define READING_LIST_SIZE 15
 
-// A "linked list" class for keeping readings
-class ReadingElement{
-    public:
-        bool containsData = false; // Since C++ object cannot be NULL
-        bool hasEntryAfterInList = false; // Since C++ object cannot be NULL
-        ReadingElement * next;
-};
-
-// Child classes for the data types, as no support for "auto" in classes :(
-class ReadingElementBool : public ReadingElement {
-    public:
-        bool data;
-};
-class ReadingElementUnsignedLong : public ReadingElement {
-    public:
-        unsigned long data;
-};
-class ReadingElementInt : public ReadingElement {
-    public:
-        int data;
-};
-
 // Class for interacting with the readings
 class Readings{
     private:
-        bool lastPIRReading;
-        ReadingElementBool readingsPIR;
-        ReadingElementUnsignedLong readingsUltrasonic;
-        ReadingElementInt readingsLDR;
-        void UpdateList(ReadingElement * list, 
-                        ReadingElement * toBeAdded);
+        
     public:
         Readings();
-        void AddPIRReading(bool reading);
-        void AddUltrasonicReading(unsigned long reading);
-        void AddLDRReading(int reading);
-        ReadingElementBool GetReadingsPIR();
-        ReadingElementUnsignedLong GetReadingsUltrasonic();
-        ReadingElementInt GetReadingsLDR();
 };
 
 #endif
