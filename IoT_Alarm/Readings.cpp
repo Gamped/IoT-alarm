332e7fc88b4c0f04b6e60022c6fbcb28b80edb3e
Gamped <mathiasgam@gmail.com>
2019-12-11 17:43:34 +0100

    Redo readings class

--- IoT_Alarm/Readings.cpp
+++ IoT_Alarm/Readings.cpp
@@ -6,3 +6,26 @@
 
 // Constructors
 Readings::Readings(){}
+
+// Set the last PIR reading to reading
+void Readings::ReadingPIR(bool reading){
+    Readings::lastPIRReading = reading;
+}
+
+// Save the last ultrasonic reading, and move pointer to next array index
+void Readings::ReadingUltrasonic(unsigned long reading){
+    Readings::readingsUltrasonic[Readings::nextUltrasonicIndex] = reading;
+    
+    if (Readings::nextUltrasonicIndex >= ((int)READING_LIST_SIZE - 1)){
+        Readings::nextUltrasonicIndex = 0;
+    } else Readings::nextUltrasonicIndex++;
+}
+
+// Save the last LDR reading, and move pointer to next array index
+void Readings::ReadingLDR(int reading){
+    Readings::readingsLDR[Readings::nextLDRIndex] = reading;
+    
+    if (Readings::nextLDRIndex >= ((int)READING_LIST_SIZE - 1)){
+        Readings::nextLDRIndex = 0;
+    } else Readings::nextLDRIndex++;
+}
--- IoT_Alarm/Readings.h
+++ IoT_Alarm/Readings.h
@@ -12,9 +12,15 @@
 // Class for interacting with the readings
 class Readings{
     private:
-        
+        bool lastPIRReading;
+        unsigned long readingsUltrasonic[READING_LIST_SIZE];
+        int readingsLDR[READING_LIST_SIZE];
+        int nextUltrasonicIndex = 0, nextLDRIndex = 0;
     public:
         Readings();
+        void ReadingPIR(bool reading);
+        void ReadingUltrasonic(unsigned long reading);
+        void ReadingLDR(int reading);
 };
 
 #endif
